5f62b573e2438447365e92589fd463d151886d89
patel jeel <42006763+jeel2308@users.noreply.github.com>
2020-07-10 21:15:04 +0530

    Updates

---
+++ precache-manifest.77b41961eac296cd86d57a4cc2b0a990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8b92cfc082dc1bd6a3d138256a3379",
+    "url": "/Admin_Panel/index.html"
+  },
+  {
+    "revision": "73376e7d00292a4344ab",
+    "url": "/Admin_Panel/static/css/main.d925701d.chunk.css"
+  },
+  {
+    "revision": "2572a86e412f184ce03e",
+    "url": "/Admin_Panel/static/js/2.142de4ad.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/Admin_Panel/static/js/2.142de4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73376e7d00292a4344ab",
+    "url": "/Admin_Panel/static/js/main.53accea0.chunk.js"
+  },
+  {
+    "revision": "e418022687a2f85f3863",
+    "url": "/Admin_Panel/static/js/runtime-main.260987c3.js"
+  }
+]);
